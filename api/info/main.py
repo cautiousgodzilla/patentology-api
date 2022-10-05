f591470f10df4bade9af71c7301e345bedfb53f1
cautiousgodzilla <92043508+cautiousgodzilla@users.noreply.github.com>
2022-10-05 23:09:03 +0530

    Add files via upload

--- api/info/main.py
+++ api/info/main.py
@@ -1,12 +1,17 @@
 # INFO
-
+import sys
+sys.path.append('..') 
 from api.common.incoming import Request
-from parse import Parser
+from parser import Parser
 from query import Query
 
 def info_handler(arguments):
     payload = Request(arguments, endpoint='info').parse()
     url = Query(payload).generate_url()
+    print(url)
     results = Parser(url)
     results_dict = results.parse()
     return results_dict
+
+x = {'id':'2894056'}
+print(info_handler(x))
